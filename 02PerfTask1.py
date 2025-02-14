2aca5ae21fb057edb9f36661648f3720c674c6d8
keroriii <estrellaiankyne@gmail.com>
2025-02-14 04:08:09 +0000

    assignments

---
+++ 02PerfTask1.py
@@ -0,0 +1,26 @@
+# 1
+#  print("Hanggang sa huli, palagi")
+#  print("Pipiliin kong maging sa 'yo") 
+3
+# 2
+#  print("Hanggang sa huli, palagi")
+#  print()
+#  print("Pipiliin kong maging sa 'yo") 
+
+# 3
+#  print("Hanggang sa huli, palagi" , "\nPipiliin kong maging sa 'yo") 
+
+# 4
+#  print("Hanggang" , " sa huli," " palagi" , "\nPipiliin kong maging sa 'yo") 
+
+# 5
+#  print("Hanggang sa huli, palagi" , end" ")
+#  print("\nPipiliin kong maging sa 'yo") 
+
+#6
+#  print("Hanggang sa huli, palagi" , end" ")
+#  print("\nPipiliin kong maging sa 'yo" , sep="-----" )
+
+#7
+#  print("Hanggang sa huli, palagi" , sep="-" , end" ")
+#  print("\nPipiliin kong maging sa 'yo" , sep="-----" )
